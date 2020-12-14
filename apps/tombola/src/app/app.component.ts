09400d2618240f7cfb738b6ed0a5f333ebb78a44
Matteo <matteo.polito@gmail.com>
2020-12-14 09:36:46 +0100

    initial commit

---
+++ apps/tombola/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tombola-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tombola';
+}
